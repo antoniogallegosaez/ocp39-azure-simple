eda639900cf6e0c991374481c84caf623668f453
ages <ages@agespc2.ageslab.com>
2018-05-08 18:45:37 +0200

    config file for ansible

--- scripts/deployOpenShift.sh
+++ scripts/deployOpenShift.sh
@@ -42,15 +42,31 @@ echo "Generating Private Keys"
 runuser -l $SUDOUSER -c "echo \"$PRIVATEKEY\" > ~/.ssh/id_rsa"
 runuser -l $SUDOUSER -c "chmod 600 ~/.ssh/id_rsa*"
 
-echo "Configuring SSH ControlPath to use shorter path name"
 
-sed -i -e "s/^# control_path = %(directory)s\/%%h-%%r/control_path = %(directory)s\/%%h-%%r/" /etc/ansible/ansible.cfg
-sed -i -e "s/^#host_key_checking = False/host_key_checking = False/" /etc/ansible/ansible.cfg
-sed -i -e "s/^#pty=False/pty=False/" /etc/ansible/ansible.cfg
+# Create ansible config file
+cat > /etc/ansible/ansible.cfg <<EOF
+[defaults]
+forks = 20
+host_key_checking = False
+remote_user = root
+roles_path = roles/
+gathering = smart
+fact_caching = jsonfile
+fact_caching_connection = $HOME/ansible/facts
+fact_caching_timeout = 600
+log_path = $HOME/ansible.log
+nocows = 1
+callback_whitelist = profile_tasks
 
-echo "Enable ansible logging"
+[privilege_escalation]
+become = False
 
-sed -i -e "s/^#log_path =/log_path =/" /etc/ansible/ansible.cfg
+[ssh_connection]
+ssh_args = -o ControlMaster=auto -o ControlPersist=600s
+control_path = %(directory)s/%%h-%%r
+pipelining = True
+timeout = 10
+EOF
 
 # Create Ansible Playbook for Post Installation task
 echo $(date) " - Create Ansible Playbook for Post Installation task"
